ca452890e1f22c7910d590c6470ba63d1bbd487d
Fritz <fritz@icloud.com>
2022-06-04 18:39:08 -0500

    first commit

---
+++ Spreadsheet Cleaner/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  Spreadsheet Cleaner
+//
+//  Created by FGT MAC on 6/4/22.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
