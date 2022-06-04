73af4145cfcce6712cfbff1d90311ea284aff30c
Fritz <fritz@icloud.com>
2022-06-04 18:44:23 -0500

    add main VC

---
+++ Spreadsheet Cleaner/View Controllers/SpreadSheetViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SpreadSheetViewController.swift
+//  Spreadsheet Cleaner
+//
+//  Created by FGT MAC on 6/4/22.
+//
+
+import Cocoa
+
+class SpreadSheetViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
